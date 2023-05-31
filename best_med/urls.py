2a02f6e2dfef448062ab47b1141133e282dcf846
Andrei Gusarenko <moto.adg@gmail.com>
2023-05-31 05:15:27 +0500

    Update urls.py in folder 'best_med/'

--- best_med/urls.py
+++ best_med/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from medservice.views import *
+from userapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view),
     path('contacts/', contacts_view),
     path('success/', success_result_view),
+    path('registration/', UserRegistrationView.as_view(), name="UserRegistration"),
+    path('login/', UserLoginView.as_view(), name="UserLogin"),
+    path('logout/', UserLogoutView.as_view(), name="UserLogout"),
 
     # Пути для создания категорий, видов, подвидов услуг и специальной подготовки
     path('create/',
@@ -31,19 +35,47 @@ urlpatterns = [
              path('med-service/', MedServiceCreateView.as_view(), name="create-MedService"),
              path('sub-med-service/', SubMedServiceCreateView.as_view(), name="create-SubMedService"),
              path('preparation/', PreparationCreateView.as_view(), name="create-Preparation"),
+             path('med-specialty/', MedSpecialtyCreateView.as_view(), name="create-MedSpecialty"),
+             path('doctor/', DoctorCreateView.as_view(), name="create-Doctor"),
              ]
          )
     ),
+
     # Пути относятся к специальной подготовке
     path('preparation/',
          include(
              [
-             path('', PreparationListView.as_view()),
+             path('', PreparationListView.as_view(), name="Preparation"),
              path('<int:pk>/update/', PreparationUpdateView.as_view()),
              path('<int:pk>/delete/', PreparationDeleteView.as_view()),
              ]
          )
     ),
+
+    # Пути относятся к медицинской специальности (медицинскому направлению)
+    path('med-specialty/',
+         include(
+             [
+             path('', MedSpecialtyListView.as_view(), name="MedSpecialty"),
+             path('<slug:short_name>/', MedSpecialtyDetailView.as_view()),
+             path('<int:pk>/update/', MedSpecialtyUpdateView.as_view()),
+             path('<int:pk>/delete/', MedSpecialtyDeleteView.as_view()),
+             ]
+         )
+    ),
+
+    # Пути относятся к врачам / докторам
+    path('doctor/',
+         include(
+             [
+             path('', DoctorListView.as_view(), name="Doctor"),
+             path('<int:pk>/', DoctorDetailView.as_view()),
+             path('<int:pk>/update/', DoctorUpdateView.as_view()),
+             path('<int:pk>/delete/', DoctorDeleteView.as_view()),
+             ]
+         )
+    ),
+
     # Пути для медицинских услуг - отображение, обновление, удаление
     path('service/',
          include(

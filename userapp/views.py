4a697643d08a39fdfa834634e4e2b090ec7361d4
Andrei Gusarenko <moto.adg@gmail.com>
2023-05-31 05:14:56 +0500

    Update views.py in folder 'best_med/userapp/'

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import MedServiceUser
+from .forms import *
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+class UserRegistrationView(CreateView):
+    model = MedServiceUser
+    template_name = 'userapp/user_registration_form.html'
+    form_class = UserRegistrationForm
+    success_url = '/login/'
+
+class UserLoginView(LoginView):
+    template_name = 'userapp/user_login_form.html'
+    form_class = UserLoginForm
+
+
+class UserLogoutView(LogoutView):
+    pass
+
